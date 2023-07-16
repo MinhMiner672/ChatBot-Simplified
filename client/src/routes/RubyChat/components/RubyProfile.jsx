a56e8ef3f225197a8d36a3cc81030990955ab7d0
MinhPythonk <phamhoangminh672@gmail.com>
2023-07-16 23:39:26 +0700

    add discord profile link

--- client/src/routes/RubyChat/components/RubyProfile.jsx
+++ client/src/routes/RubyChat/components/RubyProfile.jsx
@@ -19,7 +19,16 @@ export default function RubyProfile() {
 					Ruby Hoshino
 				</h1>
 				<p id="author" className="text-base text-stone-400">
-					created by <b className="text-stone-300">@MinhMiner</b>
+					created by{" "}
+					<b className="text-stone-300 hover:underline">
+						<a
+							href="https://discord.com/users/751265539669622785"
+							target="_blank"
+							rel="noreferrer"
+						>
+							@MinhMiner
+						</a>
+					</b>
 				</p>
 			</div>
 		</main>
